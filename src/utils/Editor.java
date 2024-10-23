bfa7670bc9fdb51308ac51bd4c6ec22556d7a87a
DmytroHalai <halai.dmytro@lll.kpi.ua>
2024-10-23 11:21:50 +0300

    init commit

--- src/utils/Editor.java
+++ src/utils/Editor.java
@@ -2,7 +2,7 @@ package utils;
 
 import java.awt.*;
 
-public abstract class Editor {
+public abstract class Editor  {
     public abstract void onLBdown(Graphics g, int x, int y);
     public abstract void onLBup(Graphics g);
     public abstract void onMouseMove(Graphics g, int x, int y);
